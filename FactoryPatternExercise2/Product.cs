d46c356b91bc03a1cfaf1d19779d0241c16f224e
Makayla Bryan <makaylabryan@gmail.com>
2023-04-25 16:42:25 -0400

    factory design pattern

---
+++ FactoryPatternExercise2/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class Product
+    {
+        public Product() 
+        {
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Which database would you like to use: List, SQL, or Mongo?");
+            string databaseAccess = Console.ReadLine();
+
+            IDataAccess database = DataAccessFactory.GetDataAccessType (databaseAccess);
+            database.LoadData();
+            database.SaveData();
+            Console.ReadLine();
         }
     }
 }
